1f2df014a8aafe17e4acf901ff95be389af569a3
Janis Kulless <janis.kulless@accenture.com>
2020-08-13 17:24:33 +0300

    Added security group

--- server.tf
+++ server.tf
@@ -13,7 +13,47 @@ data "aws_ami" "linux_ami_hvm" {
     }
 }
 
-resource "aws_key_pair" "keypair"{
+resource "aws_key_pair" "keypair" {
     key_name = "${var.name_prefix}-key"
     public_key = "${file("${var.ssh_key_file}.pub")}"
+}
+
+resource "aws_security_group" "server_fw" {
+    vpc_id = data.aws_vpc.private_cloud.id
+    name = "${var.name_prefix}-server-fw"
+
+    ingress {
+        protocol = "tcp"
+        from_port = 8080
+        to_port = 8080
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        protocol = "icmp"
+        from_port = -1
+        to_port = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        protocol = "tcp"
+        from_port = 22
+        to_port = 22
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        protocol = "-1"
+        from_port = 0
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = "${merge(
+        local.default_tags,
+        map(
+            "name", "${var.name_prefix}-server-fw"
+        )
+    )}"
 }
